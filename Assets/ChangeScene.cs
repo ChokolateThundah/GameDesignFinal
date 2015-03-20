93ecafe9e1ee85fe070d3305fc4a2266be5ca9f9
Jocday Macias <jmacias2013@my.fit.edu>
2015-03-20 19:07:02 -0400

    UI update
    UI update, buttons work to cycle through menu. Still have to add
    functionality to buttons.

---
+++ Assets/ChangeScene.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour {
+
+	// Change to desired scene
+	public void ChangeToScene (int sceneToChangeTo) {
+		Application.LoadLevel(sceneToChangeTo);
+	}
+}
